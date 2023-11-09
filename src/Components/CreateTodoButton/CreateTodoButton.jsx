02be866af841b80d1e59bb0aae8d2ef5fc661187
GiovanniAlzate <galzate@cesde.edu.co>
2023-11-09 10:56:21 -0500

    OnClick + OnChange

--- src/Components/CreateTodoButton/CreateTodoButton.jsx
+++ src/Components/CreateTodoButton/CreateTodoButton.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function CreateTodoButton() {
     return (
-        <button className='w-16 h-16 rounded-full text-5xl flex justify-center items-center bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500'>+</button>
+        <button className='w-16 h-16 rounded-full text-5xl font-bold grid place-items-center bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500
+        transition-transform rotate-0 hover:rotate-180'
+            onClick={(event) => { console.log(event.target) }}>
+            +
+        </button>
     )
 }
 
--- src/Components/TodoSearch/TodoSearch.jsx
+++ src/Components/TodoSearch/TodoSearch.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function TodoSearch() {
     return (
         <div className='w-5/6'>
-            <input className="w-full h-12 text-2xl p-2 border rounded-lg focus:outline-orange-700 shadow-xl" type="text" placeholder="Write a task..."/>
+            <input className="w-full h-12 text-2xl p-2 border rounded-lg focus:outline-orange-700 shadow-xl" type="text" placeholder="Write a task..."
+                onChange={(event) => {
+                    console.log(event.target.value)
+                }} />
         </div>
     )
 }
