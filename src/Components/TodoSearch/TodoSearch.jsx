95a52ba5577dc3a30aa81216a4e88f905dbd56e6
GiovanniAlzate <galzate@cesde.edu.co>
2023-11-09 11:22:29 -0500

    Uso del estado

--- src/Components/TodoSearch/TodoSearch.jsx
+++ src/Components/TodoSearch/TodoSearch.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = useState('');
+    //console.log("Los usuarios buscan todos de: ", searchValue)
+
     return (
         <div className='w-5/6'>
             <input className="w-full h-12 text-2xl p-2 border rounded-lg focus:outline-orange-700 shadow-xl" type="text" placeholder="Write a task..."
+                value={searchValue}
                 onChange={(event) => {
-                    console.log(event.target.value)
+                    setSearchValue(event.target.value)
+                    //console.log(event.target.value)
                 }} />
         </div>
     )
